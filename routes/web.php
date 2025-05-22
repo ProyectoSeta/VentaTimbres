7173738971c17c7d6aafd4787cf47263b6b6d254
ProyectoSeta <proyectos.seta.2024@gmail.com>
2025-05-22 17:08:49 -0400

    permisos

--- routes/web.php
+++ routes/web.php
@@ -83,13 +83,6 @@ Route::get('/reporte_anual/pdf_reporte/{year?}/', [App\Http\Controllers\ReporteA
 
 
 
-
-
-
-
-
-
-
 ///////////////////// PAPEL DE SEGURIDAD
 Route::get('/emision_papel', [App\Http\Controllers\PapelSeguridadController::class, 'index'])->name('emision_papel');
 Route::post('/papel/modal_f14', [App\Http\Controllers\PapelSeguridadController::class, 'modal_f14'])->name('papel.modal_f14');
@@ -137,6 +130,47 @@ Route::get('/emision_ucd/pdf_emision', [App\Http\Controllers\EmisionUcdControlle
 
 
 
+
+
+
+///////////////////// ASIGNACIÓN DE TIMBRES
+Route::get('/asignar', [App\Http\Controllers\AsignarController::class, 'index'])->name('asignar');
+Route::post('/asignar/taquillas', [App\Http\Controllers\AsignarController::class, 'taquillas'])->name('asignar.taquillas');
+Route::post('/asignar/funcionario', [App\Http\Controllers\AsignarController::class, 'funcionario'])->name('asignar.funcionario');
+Route::post('/asignar/asignar_forma_14', [App\Http\Controllers\AsignarController::class, 'asignar_forma_14'])->name('asignar.asignar_forma_14');
+Route::post('/asignar/content_estampillas', [App\Http\Controllers\AsignarController::class, 'content_estampillas'])->name('asignar.content_estampillas');
+Route::post('/asignar/denominacions', [App\Http\Controllers\AsignarController::class, 'denominacions'])->name('asignar.denominacions');
+Route::post('/asignar/asignar_estampillas', [App\Http\Controllers\AsignarController::class, 'asignar_estampillas'])->name('asignar.asignar_estampillas');
+Route::post('/asignar/info_taquilla', [App\Http\Controllers\AsignarController::class, 'info_taquilla'])->name('asignar.info_taquilla');
+Route::post('/asignar/detalle_estampillas', [App\Http\Controllers\AsignarController::class, 'detalle_estampillas'])->name('asignar.detalle_estampillas');
+Route::post('/asignar/detalle_rollos', [App\Http\Controllers\AsignarController::class, 'detalle_rollos'])->name('asignar.detalle_rollos');
+/////////PDF: CONSTANCIA DE ASIGNACIÓN
+Route::get('/asignar/pdf_forma14', [App\Http\Controllers\AsignarController::class, 'pdf_forma14'])->name('asignar.pdf_forma14');
+Route::get('/asignar/pdf_estampillas', [App\Http\Controllers\AsignarController::class, 'pdf_estampillas'])->name('asignar.pdf_estampillas');
+
+
+
+///////////////////// ASIGNACIONES DE TAQUILLAS
+Route::get('/timbres_asignados', [App\Http\Controllers\AsignadoTaquillasController::class, 'index'])->name('timbres_asignados');
+Route::post('/timbres_asignados/modal_forma14', [App\Http\Controllers\AsignadoTaquillasController::class, 'modal_forma14'])->name('timbres_asignados.modal_forma14');
+Route::post('/timbres_asignados/recibido_forma14', [App\Http\Controllers\AsignadoTaquillasController::class, 'recibido_forma14'])->name('timbres_asignados.recibido_forma14');
+Route::post('/timbres_asignados/modal_estampillas', [App\Http\Controllers\AsignadoTaquillasController::class, 'modal_estampillas'])->name('timbres_asignados.modal_estampillas');
+Route::post('/timbres_asignados/recibido_estampillas', [App\Http\Controllers\AsignadoTaquillasController::class, 'recibido_estampillas'])->name('timbres_asignados.recibido_estampillas');
+
+
+///////////////////// HISTORIAL ASIGNACIONES
+Route::get('/historial_asignaciones', [App\Http\Controllers\HistorialAsignacionesController::class, 'index'])->name('historial_asignaciones');
+
+
+
+
+
+
+
+
+
+
+
 //////////////////////  VENTA FORMA 14 - ESTAMPILLAS
 Route::get('/venta', [App\Http\Controllers\VentaController::class, 'index'])->name('venta');
 Route::post('/venta/search', [App\Http\Controllers\VentaController::class, 'search'])->name('venta.search');
@@ -168,59 +202,31 @@ Route::post('/venta/venta', [App\Http\Controllers\VentaController::class, 'venta
 
 
 ///////////////////// EMISIÓN ROLLOS FORMA 14
-Route::get('/emision_rollos', [App\Http\Controllers\RollosController::class, 'index'])->name('emision_rollos');
-Route::post('/rollos/modal_emitir', [App\Http\Controllers\RollosController::class, 'modal_emitir'])->name('rollos.modal_emitir');
-Route::post('/rollos/emitir', [App\Http\Controllers\RollosController::class, 'emitir'])->name('rollos.emitir');
-Route::post('/rollos/modal_enviar', [App\Http\Controllers\RollosController::class, 'modal_enviar'])->name('rollos.modal_enviar');
-Route::post('/rollos/detalles', [App\Http\Controllers\RollosController::class, 'detalles'])->name('rollos.detalles');
-Route::post('/rollos/enviar_inventario', [App\Http\Controllers\RollosController::class, 'enviar_inventario'])->name('rollos.enviar_inventario');
-Route::post('/rollos/delete', [App\Http\Controllers\RollosController::class, 'delete'])->name('rollos.delete');
-/////////PDF: ROLLOS A EMITIR
-Route::get('/rollos/pdf', [App\Http\Controllers\RollosController::class, 'pdf'])->name('rollos.pdf');
+// Route::get('/emision_rollos', [App\Http\Controllers\RollosController::class, 'index'])->name('emision_rollos');
+// Route::post('/rollos/modal_emitir', [App\Http\Controllers\RollosController::class, 'modal_emitir'])->name('rollos.modal_emitir');
+// Route::post('/rollos/emitir', [App\Http\Controllers\RollosController::class, 'emitir'])->name('rollos.emitir');
+// Route::post('/rollos/modal_enviar', [App\Http\Controllers\RollosController::class, 'modal_enviar'])->name('rollos.modal_enviar');
+// Route::post('/rollos/detalles', [App\Http\Controllers\RollosController::class, 'detalles'])->name('rollos.detalles');
+// Route::post('/rollos/enviar_inventario', [App\Http\Controllers\RollosController::class, 'enviar_inventario'])->name('rollos.enviar_inventario');
+// Route::post('/rollos/delete', [App\Http\Controllers\RollosController::class, 'delete'])->name('rollos.delete');
+// /////////PDF: ROLLOS A EMITIR
+// Route::get('/rollos/pdf', [App\Http\Controllers\RollosController::class, 'pdf'])->name('rollos.pdf');
 
 
 
-//////////////////EMISIÓN ESTAMPILLAS
-Route::get('/emision_estampillas', [App\Http\Controllers\EstampillasController::class, 'index'])->name('emision_estampillas');
-Route::post('/emision_estampillas/denominacions', [App\Http\Controllers\EstampillasController::class, 'denominacions'])->name('emision_estampillas.denominacions');
-Route::post('/emision_estampillas/modal_emitir', [App\Http\Controllers\EstampillasController::class, 'modal_emitir'])->name('emision_estampillas.modal_emitir');
-Route::post('/emision_estampillas/emitir_estampillas', [App\Http\Controllers\EstampillasController::class, 'emitir_estampillas'])->name('emision_estampillas.emitir_estampillas');
-Route::post('/emision_estampillas/detalles', [App\Http\Controllers\EstampillasController::class, 'detalles'])->name('emision_estampillas.detalles');
-Route::post('/emision_estampillas/modal_enviar', [App\Http\Controllers\EstampillasController::class, 'modal_enviar'])->name('emision_estampillas.modal_enviar');
-Route::post('/emision_estampillas/enviar_inventario', [App\Http\Controllers\EstampillasController::class, 'enviar_inventario'])->name('emision_estampillas.enviar_inventario');
-Route::post('/emision_estampillas/delete', [App\Http\Controllers\EstampillasController::class, 'delete'])->name('emision_estampillas.delete');
-/////////PDF: TIRAS A EMITIR 
-Route::get('/emision_estampillas/pdf', [App\Http\Controllers\EstampillasController::class, 'pdf'])->name('emision_estampillas.pdf');
+// //////////////////EMISIÓN ESTAMPILLAS
+// Route::get('/emision_estampillas', [App\Http\Controllers\EstampillasController::class, 'index'])->name('emision_estampillas');
+// Route::post('/emision_estampillas/denominacions', [App\Http\Controllers\EstampillasController::class, 'denominacions'])->name('emision_estampillas.denominacions');
+// Route::post('/emision_estampillas/modal_emitir', [App\Http\Controllers\EstampillasController::class, 'modal_emitir'])->name('emision_estampillas.modal_emitir');
+// Route::post('/emision_estampillas/emitir_estampillas', [App\Http\Controllers\EstampillasController::class, 'emitir_estampillas'])->name('emision_estampillas.emitir_estampillas');
+// Route::post('/emision_estampillas/detalles', [App\Http\Controllers\EstampillasController::class, 'detalles'])->name('emision_estampillas.detalles');
+// Route::post('/emision_estampillas/modal_enviar', [App\Http\Controllers\EstampillasController::class, 'modal_enviar'])->name('emision_estampillas.modal_enviar');
+// Route::post('/emision_estampillas/enviar_inventario', [App\Http\Controllers\EstampillasController::class, 'enviar_inventario'])->name('emision_estampillas.enviar_inventario');
+// Route::post('/emision_estampillas/delete', [App\Http\Controllers\EstampillasController::class, 'delete'])->name('emision_estampillas.delete');
+// /////////PDF: TIRAS A EMITIR 
+// Route::get('/emision_estampillas/pdf', [App\Http\Controllers\EstampillasController::class, 'pdf'])->name('emision_estampillas.pdf');
 
 
-
-///////////////////// ASIGNACIÓN ROLLOS FORMA 14
-Route::get('/asignar', [App\Http\Controllers\AsignarController::class, 'index'])->name('asignar');
-Route::post('/asignar/taquillas', [App\Http\Controllers\AsignarController::class, 'taquillas'])->name('asignar.taquillas');
-Route::post('/asignar/funcionario', [App\Http\Controllers\AsignarController::class, 'funcionario'])->name('asignar.funcionario');
-Route::post('/asignar/asignar_forma_14', [App\Http\Controllers\AsignarController::class, 'asignar_forma_14'])->name('asignar.asignar_forma_14');
-Route::post('/asignar/content_estampillas', [App\Http\Controllers\AsignarController::class, 'content_estampillas'])->name('asignar.content_estampillas');
-Route::post('/asignar/denominacions', [App\Http\Controllers\AsignarController::class, 'denominacions'])->name('asignar.denominacions');
-Route::post('/asignar/asignar_estampillas', [App\Http\Controllers\AsignarController::class, 'asignar_estampillas'])->name('asignar.asignar_estampillas');
-Route::post('/asignar/info_taquilla', [App\Http\Controllers\AsignarController::class, 'info_taquilla'])->name('asignar.info_taquilla');
-Route::post('/asignar/detalle_estampillas', [App\Http\Controllers\AsignarController::class, 'detalle_estampillas'])->name('asignar.detalle_estampillas');
-Route::post('/asignar/detalle_rollos', [App\Http\Controllers\AsignarController::class, 'detalle_rollos'])->name('asignar.detalle_rollos');
-/////////PDF: CONSTANCIA DE ASIGNACIÓN
-Route::get('/asignar/pdf_forma14', [App\Http\Controllers\AsignarController::class, 'pdf_forma14'])->name('asignar.pdf_forma14');
-Route::get('/asignar/pdf_estampillas', [App\Http\Controllers\AsignarController::class, 'pdf_estampillas'])->name('asignar.pdf_estampillas');
-
-
-
-///////////////////// ASIGNACIONES DE TAQUILLAS
-Route::get('/timbres_asignados', [App\Http\Controllers\AsignadoTaquillasController::class, 'index'])->name('timbres_asignados');
-Route::post('/timbres_asignados/modal_forma14', [App\Http\Controllers\AsignadoTaquillasController::class, 'modal_forma14'])->name('timbres_asignados.modal_forma14');
-Route::post('/timbres_asignados/recibido_forma14', [App\Http\Controllers\AsignadoTaquillasController::class, 'recibido_forma14'])->name('timbres_asignados.recibido_forma14');
-Route::post('/timbres_asignados/modal_estampillas', [App\Http\Controllers\AsignadoTaquillasController::class, 'modal_estampillas'])->name('timbres_asignados.modal_estampillas');
-Route::post('/timbres_asignados/recibido_estampillas', [App\Http\Controllers\AsignadoTaquillasController::class, 'recibido_estampillas'])->name('timbres_asignados.recibido_estampillas');
-
-
-///////////////////// HISTORIAL ASIGNACIONES
-Route::get('/historial_asignaciones', [App\Http\Controllers\HistorialAsignacionesController::class, 'index'])->name('historial_asignaciones');
 
 
 
